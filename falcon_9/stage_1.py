88999ea1a326a15a34fbc068a34a06f9f9a52f60
Mike Bernard <mike.bernard@uconn.edu>
2021-07-29 21:40:51 -0700

    move classes to Falcon_9.py

--- falcon_9/stage_1.py
+++ falcon_9/stage_1.py
@@ -1,55 +1,9 @@
 import enum
 
+import krpc
 
-class Stage_1_Profile(enum.Enum):
-    """Ascent/landing profiles for Falcon 9 Stage 1."""
-    RTLS = 0
+from Falcon_9 import Falcon_9
 
-
-class Stage_1_RTLS_Mode(enum.Enum):
-    """
-    Modes for Falcon 9 Stage 1 with a mission profile that returns
-    to the launch site (RTLS).
-    """
-    PAD_PASSIVE = 0  # pre-flight; don't do anything
-    PAD_ACTIVE = 1  # ignite engines, decouple launch tower
-    ASCENT_VERTICAL = 2  # ascend vertically
-    ASCENT_GRAVITY_TURN = 3  # ascend with gravity turn
-    POST_SEP_HOLD = 4  # separation hold
-    BOOSTBACK_FLIP = 5  # boostback flip
-    BOOSTBACK = 6  # boostback burn
-    ENTRY_FLIP = 7  # entry flip
-    ENTRY_HOLD = 8  # entry hold
-    LANDING = 9  # land!
-    TOUCHDOWN_PASSIVE = 10  # post-landing; don't do anything
-
-    ABORT = 11  # aborted launch! shutdown everything
-
-
-class Stage_1():
-    """Attributes and methods for a Falcon 9 first stage."""
-    
-    def __init__(self, vessel, profile):
-        """
-        Instantiate a Stage_1 object.
-
-        Args:
-            vessel (krpc.Vessel): the first stage of the rocket
-            profile (Stage_1_Profile): the ascent/landing profile to use
-        """
-        self.vessel = vessel
-        self.profile = profile
-        self._determine_init_mode()  # sets self.mode
-
-    def _determine_init_mode(self):
-        """
-        Determine which mode enumeration to use initially
-        based on flight profile.
-        """
-        if self.profile == Stage_1_Profile.RTLS:
-            self.mode = Stage_1_RTLS_Mode.PAD_PASSIVE
-        else:
-            self.mode = None
 
 
 def perform_pad_passive_ops():
@@ -68,7 +22,20 @@ def perform_pad_active_ops():
 
 
 def main():
-    pass
+    """
+    Launch the rocket and control Stage 1.
+    Assumes starting with Kerbal Space Program loaded, with the Falcon 9
+    craft loaded and siting passively on the launchpad.
+    """
+    conn = krpc.connect()
+    space_center_api = conn.space_center
+    launch_vessel = space_center_api.active_vessel
+
+    f9_stack = Falcon_9(launch_vessel)
+
+    mission_ongoing = True
+    while mission_ongoing:
+        mission_ongoing = f9_stack.main()
 
 
 if __name__ == '__main__':
