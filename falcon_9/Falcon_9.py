7af414565ee0658314f739164d0dcd9ba4f2f834
Mike Bernard <mike.bernard@uconn.edu>
2021-07-29 22:49:41 -0700

    implement initial three modes for F9 S1

--- falcon_9/Falcon_9.py
+++ falcon_9/Falcon_9.py
@@ -1,13 +1,12 @@
 """Classes of objects used across a Falcon 9 launch vehicle."""
 
 import enum
+import time
 
 import krpc
+import numpy as np
 
-
-class Stage_1_Profile(enum.Enum):
-    """Ascent/landing profiles for Falcon 9 Stage 1."""
-    RTLS = 0
+import constants
 
 
 class Stage_1_RTLS_Mode(enum.Enum):
@@ -49,43 +48,83 @@ class Falcon_9:
             'F9_S1_' + tag: vessel_tags_and_parts['F9_S1_' + tag]
             for tag in stage_1_tags
         }
-        self.stage_1 = Stage_1(stage_1_tags_and_parts, Stage_1_Profile.RTLS)
+        self.stage_1 = Stage_1(stage_1_tags_and_parts)
 
         stage_2_tags_and_parts = {}
+        self.stage_2 = Stage_2()
+
+    def main(self, space_center_api):
+        status = self.stage_1.main(space_center_api)
+        status = self.stage_2.main(space_center_api, status)
+        return status
 
 
 class Stage_1:
     """Attributes and methods for a Falcon 9 first stage."""
 
-    CONTROL_PART_TAG = 'F9_S1_Interstage'
+    PART_PREFIX = 'F9_S1_'
+    CONTROL_PART_TAG = PART_PREFIX + 'Interstage'
+    CENTER_ENGINE_TAG = PART_PREFIX + 'Engine_1'
 
-    def __init__(self, tag_parts_dict, profile):
+    def __init__(self, tag_parts_dict):
         """
         Instantiate a Stage_1 object.
 
         Args:
             tag_parts_dict (dict of {str: krpc.Part}): Part objects on stage 1
-            profile (Stage_1_Profile): the ascent/landing profile to use
         """
         self.parts = tag_parts_dict
-        self.profile = profile
         self.control_part = self.parts[self.CONTROL_PART_TAG]
         self.vessel = self.control_part.vessel
-        self._determine_init_mode()  # sets self.mode
+        self.mode = Stage_1_RTLS_Mode.PAD_PASSIVE
 
-    def _determine_init_mode(self):
-        """
-        Determine which mode enumeration to use initially
-        based on flight profile.
-        """
-        if self.profile == Stage_1_Profile.RTLS:
-            self.mode = Stage_1_RTLS_Mode.PAD_PASSIVE
+    def main(self, space_center_api):
+        status = True
+        if self.mode == Stage_1_RTLS_Mode.PAD_PASSIVE:
+            print('Falcon 9 is in startup.')
+            for i in range(10):
+                print(f'T-{10 - i}')
+                time.sleep(1)
+            self.mode = Stage_1_RTLS_Mode.PAD_ACTIVE
+            print('Stage 1 Mode Change: PAD_ACTIVE')
+
+        elif self.mode == Stage_1_RTLS_Mode.PAD_ACTIVE:
+            # TODO: turn on SAS and set initial control pitch and heading before ignition
+            self.vessel.control.sas = True
+            self.vessel.control.throttle = 1.0
+            self.vessel.control.activate_next_stage()
+            time.sleep(constants.DT_COMMAND)
+            self.vessel.control.activate_next_stage()
+            print('Liftoff!')
+
+            while not (np.linalg.norm(
+                self.parts[self.CENTER_ENGINE_TAG].center_of_mass(
+                    space_center_api.bodies['Kerbin'].reference_frame
+                )) > constants.R0_DRAGON):
+                time.sleep(0.1)
+            print('Tower cleared!')
+
+            self.mode = Stage_1_RTLS_Mode.ASCENT_VERTICAL
+            print('Stage 1 Mode Change: ASCENT_VERTICAL')
+
+        elif self.mode == Stage_1_RTLS_Mode.ASCENT_VERTICAL:
+            if np.linalg.norm(self.vessel.flight(
+                space_center_api.bodies['Kerbin'].reference_frame
+            ).velocity) > 100:
+                self.mode = Stage_1_RTLS_Mode.ASCENT_GRAVITY_TURN
+                print('Stage 1 Mode Change: ASCENT_GRAVITY_TURN')
+
         else:
-            self.mode = None
+            status = False
 
+        return status
 
 class Stage_2:
     """Attributes and methods for a Falcon 9 second stage."""
 
-    def __init__(self, control_part, profile):
+    def __init__(self, tag_parts_dict=None, profile=None):
+        # TODO: remove default None from args once __init__ is implemented
         pass
+
+    def main(self, space_center_api, stage_1_status):
+        return stage_1_status
